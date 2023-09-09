57191ab0605faa5f3735babacc43fe4753a1d9aa
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-09-09 12:35:54 -0700

    added react and express

--- back_end/config/config.js
+++ back_end/config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 module.exports =
 {
   "development": {
-    "username": "postgres",
-    "password": "AngieSQL",
-    "database": "pristine_paws",
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
